5c3bfb2285424fa39e42a754cbb8630e156e5422
Kapil3003 <41763456+Kapil3003@users.noreply.github.com>
2023-09-29 19:08:36 +0530

    Updated Links

--- OI_Analysis_Streamlit_cloud.py
+++ OI_Analysis_Streamlit_cloud.py
@@ -255,7 +255,7 @@ st.divider()
 "## Code"
 
 "For detail code refer my github repo :- "
-" 1. [Predictive Analysis of Stock Trajectories using Geometric Brownian Motion](https://github.com/Kapil3003/Quant_Project_1/blob/main/Project_1_GBM.ipynb)"
+" 1. [[webApp]](https://quantproject1-csovwwndasw9kuk2vpygjp.streamlit.app/) [Predictive Analysis of Stock Trajectories using Geometric Brownian Motion](https://github.com/Kapil3003/Quant_Project_1/blob/main/Project_1_GBM.ipynb)"
 
 " 2. [Comprehensive VaR Analysis: Methods Comparison, Backtesting, and Stress Testing](https://github.com/Kapil3003/Quant_Project_2/blob/main/Project_2_VaR_Analysis.ipynb)"
 
@@ -263,5 +263,5 @@ st.divider()
 
 " 4. [Market Volatility Forecasting: An ExtensiveComparative Study](https://github.com/Kapil3003/Quant_Project_4/blob/main/Project_4_Volatility%20Forecasting.ipynb)"
 
-" 5. [Real-Time Options Chain Data Analysis Dashboard](https://github.com/Kapil3003/Quant_Project_5)"
+" 5. [[webApp]](https://quantproject5-gcs2rtyqub8wj8osxwegu2.streamlit.app/) [Real-Time Options Chain Data Analysis Dashboard](https://github.com/Kapil3003/Quant_Project_5)"
 
